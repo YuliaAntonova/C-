1dbbc02f95de49be57ce01a75a2aaab46af20366
Yulia <edc1cde@rambler.ru>
2022-12-12 01:54:18 +0300

    Random

--- Example004_Div/Program.cs
+++ Example004_Div/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿int numberA = new Random().Next(1, 10);// 123...9
+Console.WriteLine (numberA);
+int numberB = new Random().Next(1, 10);
+Console.WriteLine ("numberB");
+int result = numberA + numberB;
+Console.Write (result);
